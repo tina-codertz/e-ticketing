762f62361dc7f13eb865b4f48e3c880cc27e3c30
christina <christinakimario8@gmail.com>
2026-02-11 20:56:25 +0300

    fixing dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+
+  optimizeDeps: {
+    include: ['react-quill'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
